2779028bbf248cfba0df0b51c5ee4f9da403548d
Antonio Figueroa <antonio_figueroa@dell.com>
2024-09-18 15:06:48 -0500

    - completes palindrome exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public bool IsAPalindrome(string word)
+		{
+			if (word == null || word  == "") return false;
+			char[] charArray = word.ToCharArray();
+			Array.Reverse(charArray);
+			string reversedWord = new string(charArray);
+
+			if (reversedWord == word)
+			{
+				return true;
+			} else
+			{
+				return false;
+			}
+		}
+	}
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,26 @@
-using System;
+﻿using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+		[Theory]
+		[InlineData("racecar", true)] // ← you will need to put some test data and expected result here.  
+		[InlineData("face", false)]
+		[InlineData("tacocat", true)]
+		[InlineData("", false)]
+		[InlineData(null, false)]
+		public void PalindromeTester(string word, bool expected)
+		{
+			//Arrange
+			var wordSmith = new WordSmith();
 
-        }
-    }
+			//Act
+			var actual = wordSmith.IsAPalindrome(word);
+
+			//Assert
+			Assert.Equal(expected, actual);
+		}
+	}
 }
